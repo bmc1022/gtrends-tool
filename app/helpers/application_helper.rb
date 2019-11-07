583f88fefd3f75e7380e13c501fb6f181623b513
Brandon Campbell <bmc1022@gmail.com>
2019-11-07 17:32:50 +0000

    layout and base partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  # returns the full title on a per-page basis
+  def full_title(page_title='')
+    base_title = "Google Trends Bulk Keyword Comparison Tool"
+    page_title.empty? ? base_title : "#{page_title} - #{base_title}"
+  end
+
 end

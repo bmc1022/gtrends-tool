b65509484537a0b0cc301179b73f99ad191525a1
Brandon Campbell <bmc1022@gmail.com>
2021-05-31 08:47:33 +0000

    base test files

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  
+  test '#svg_icon displays an inline svg reference' do
+      
+  end
+  
+end
---
+++ test/system/gtrends_test.rb
@@ -0,0 +1,30 @@
+require 'application_system_test_case'
+ 
+class GtrendsTest < ApplicationSystemTestCase
+  
+  setup do
+    
+  end
+  
+  test 'user creates new trend list' do
+    
+  end
+  
+  test 'user deletes trend list' do
+    
+  end
+  
+  test 'user copies list to clipboard' do
+    
+  end
+  
+  test 'user performs a search' do
+    
+  end
+  
+  test 'user navigates to new page using pagination' do
+    
+  end
+  
+end
+  

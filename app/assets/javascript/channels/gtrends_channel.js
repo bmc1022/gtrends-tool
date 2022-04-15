97cb314a4e4bd93dd6b3d77fa30a12974f45afc4
Brandon Campbell <bmc1022@gmail.com>
2022-04-15 22:16:11 +1000

    remove webpacker

--- app/assets/javascript/channels/gtrends_channel.js
+++
@@ -1,8 +0,0 @@
-import CableReady from 'cable_ready'
-import consumer from './consumer'
-
-consumer.subscriptions.create('GtrendsChannel', {
-  received(data) {
-    if (data.cableReady) CableReady.perform(data.operations);
-  }
-});

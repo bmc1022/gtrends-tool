8da7d5f00022c36220245ad7f2a1bbf6d2050dfa
Brandon Campbell <bmc1022@gmail.com>
2022-04-17 13:40:58 +1000

    custom debug logger

---
+++ config/initializers/loggers.rb
@@ -0,0 +1,6 @@
+if Rails.env.development? || Rails.env.test?
+  logfile = File.open(Rails.root.join('log/debug_logger.log'), 'a') # Create log file.
+  logfile.sync = true # Automatically flush data to file.
+  DEBUG_LOGGER = Logger.new(logfile) # Constant accessible anywhere.
+  DEBUG_LOGGER.level =  Logger::DEBUG
+end

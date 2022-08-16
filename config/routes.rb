c12eae4ee6ff38dcc47c85ee8167d836f335f055
Brandon Campbell <bmc1022@gmail.com>
2022-08-16 12:27:32 +1000

    update routes syntax

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
   devise_for :users, skip: [:registrations, :sessions],
                      controllers: { sessions: 'sessions' }
   devise_scope :user do
-    get  'login' => 'sessions#new', :as => :new_user_session
-    post 'login' => 'sessions#create', :as => :user_session
-    delete 'logout' => 'sessions#destroy'
+    get  'login', to: 'sessions#new', as: :new_user_session
+    post 'login', to: 'sessions#create', as: :user_session
+    delete 'logout', to: 'sessions#destroy'
   end
 
   # # disabled for demo purposes

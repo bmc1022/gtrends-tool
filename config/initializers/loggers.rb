1cb6ba18dc11d384c9b35f5d2d1bc6a3c19acefc
Brandon Campbell <bmc1022@gmail.com>
2022-08-20 14:28:37 +1000

    base rubocop offenses

--- config/initializers/loggers.rb
+++ config/initializers/loggers.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 if Rails.env.development? || Rails.env.test?
-  logfile = File.open(Rails.root.join('log/debug_logger.log'), 'a') # Create log file.
+  logfile = File.open(Rails.root.join("log/debug_logger.log"), "a") # Create log file.
   logfile.sync = true # Automatically flush data to file.
   DEBUG_LOGGER = Logger.new(logfile) # Constant accessible anywhere.
   DEBUG_LOGGER.level = Logger::DEBUG

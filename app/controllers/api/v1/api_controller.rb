a854e3007f013d65dd0f8c280f2be292158fec68
Brandon Campbell <bmc1022@gmail.com>
2023-03-22 10:55:24 +1100

    remove api references

--- app/controllers/api/v1/api_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Api
-  module V1
-    class ApiController < ActionController::Base
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: "json" } do
-    namespace :v1 do
-      resources :gtrends, only: [:index, :create, :destroy]
-    end
-  end
-
   devise_for :users, skip: [:registrations, :sessions], controllers: { sessions: "sessions" }
   devise_scope :user do
     get  "login", to: "sessions#new", as: :new_user_session

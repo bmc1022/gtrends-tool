5736c49a02c91d2bc7ea81464d1dce53b683f4e3
Brandon Campbell <bmc1022@gmail.com>
2023-03-21 23:53:52 +1100

    tooltips stimulus controller

--- app/assets/javascript/controllers/tooltip_controller.js
+++ app/assets/javascript/controllers/tooltip_controller.js
@@ -1,29 +1,24 @@
-import { Tooltip } from 'bootstrap'
+import { Controller } from "@hotwired/stimulus"
+import { Tooltip } from "bootstrap"
 
-App.Tooltip = {
-  
-  init() {
-    // initialize all bootstrap tooltips
-    new Tooltip(document.body, {
-      selector: '[data-bs-toggle="tooltip"]'
-    });
-  },
+export default class extends Controller {
+  static targets = ["tooltip"]
 
-  keywordsCopied() {
-    document.addEventListener('click', (e) => {
-      let clipboard = e.target.closest('.clipboard');
-      if (clipboard) {
-        clipboard.setAttribute('data-bs-original-title', 'Data Copied!');
-        let el = Tooltip.getInstance(clipboard);
-        el.show();
-        clipboard.setAttribute('data-bs-original-title', 'Copy Data (CSV)');
-      }
-    });
+  connect() {
+    this.tooltips = this.tooltipTargets.map((tooltip) => new Tooltip(tooltip))
   }
-  
-};
 
-document.addEventListener('turbolinks:load', () => { 
-  App.Tooltip.init();
-  App.Tooltip.keywordsCopied();
-});
+  disconnect() {
+    this.tooltips.forEach((tooltip) => tooltip.dispose())
+  }
+
+  clipboardTooltip(event) {
+    let clipboard = event.currentTarget
+    let tooltipInstance = Tooltip.getInstance(clipboard)
+    tooltipInstance.setContent({".tooltip-inner": "Copied!"})
+    setTimeout(() => {
+      tooltipInstance.hide()
+      tooltipInstance.setContent({".tooltip-inner": "Copy Data (CSV)"})
+    }, 1000)
+  }
+}

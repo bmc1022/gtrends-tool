0a46eee5b1396f61878e9fd1be88293aa94352e5
Brandon Campbell <bmc1022@gmail.com>
2023-03-25 03:20:44 +1100

    refactor callback condition

--- app/models/gtrend.rb
+++ app/models/gtrend.rb
@@ -3,13 +3,15 @@
 class Gtrend < ApplicationRecord
   has_many :keywords, dependent: :destroy, inverse_of: :gtrend
 
-  before_save :update_kws, unless: :skip_kws?
+  # :kws is a virtual/non-persisted attribute and should be skipped on updates to
+  # gtrend records since the assigned data will no longer exist after save.
+  before_save :update_kws, unless: :persisted?
 
   attribute :kws, :text
 
   validates :name, presence: true, length: { minimum: 2, maximum: 100 },
                    uniqueness: { case_sensitive: false }
-  validates :kws,  presence: true, length: { maximum: 5000 }, unless: :skip_kws?
+  validates :kws,  presence: true, length: { maximum: 5000 }, unless: :persisted?
   validate  :kw_count, on: :create
 
   def highest_5y_avg
@@ -28,11 +30,5 @@ class Gtrend < ApplicationRecord
 
   def update_kws
     self.kws = kws_to_list
-  end
-
-  # :kws is a virtual/non-persisted attribute and should be skipped on updates to
-  # gtrend records since the assigned data will no longer exist after save.
-  def skip_kws?
-    persisted?
   end
 end

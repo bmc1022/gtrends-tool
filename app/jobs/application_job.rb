8952271cf04c0562adc7ed7751805b0c4e3fe308
Brandon Campbell <bmc1022@gmail.com>
2023-04-20 03:49:49 +1000

    remove cableready logic from application_job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,7 +6,4 @@ class ApplicationJob < ActiveJob::Base
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
-
-  include CableReady::Broadcaster
-  delegate :render, to: :ApplicationController
 end

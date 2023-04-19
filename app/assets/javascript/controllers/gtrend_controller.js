eeacdab80fd030be4e34a412ee8cb1d9f58b843a
Brandon Campbell <bmc1022@gmail.com>
2023-04-20 07:51:52 +1000

    only subscribe to trends where the background job has not finished

--- app/assets/javascript/controllers/gtrend_controller.js
+++ app/assets/javascript/controllers/gtrend_controller.js
@@ -2,20 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 import CableReady from "cable_ready"
 
 export default class extends Controller {
+  static values = {
+    jobStatus: String
+  }
+
   connect() {
     const gtrendId = parseInt(this.element.id.substring("gtrend_".length), 10)
 
-    this.channel = this.application.consumer.subscriptions.create({
-      channel: 'GtrendsChannel',
-      gtrend_id: gtrendId
-    }, {
-      received (data) {
-        if (data.cableReady) CableReady.perform(data.operations)
-      },
-    })
+    if (this.jobStatusValue !== "done") {
+      this.channel = this.application.consumer.subscriptions.create({
+        channel: "GtrendsChannel",
+        gtrend_id: gtrendId
+      }, {
+        connected() {
+          console.log(`Connected to GtrendsChannel for Gtrend #${gtrendId}`)
+        },
+        disconnected() {
+          console.log(`Disconnected from GtrendsChannel for Gtrend #${gtrendId}`)
+        },
+        received (data) {
+          if (data.cableReady) CableReady.perform(data.operations)
+        },
+      })
+    }
   }
 
   disconnect() {
-    this.channel.unsubscribe()
+    if (this.channel) {
+      this.channel.unsubscribe()
+    }
   }
 }

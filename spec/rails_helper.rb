ab584e6188104ff4dd9404465022d3fcdb72a6aa
Brandon Campbell <bmc1022@gmail.com>
2023-05-12 05:05:12 +1000

    ignore localhost requests when using vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -56,6 +56,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = "spec/support/vcr_cassettes"
   config.hook_into(:webmock)
+  config.ignore_localhost = true
   config.configure_rspec_metadata!
   config.default_cassette_options = {
     record: :new_episodes,

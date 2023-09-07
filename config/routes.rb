495d1207130e255ade587070d28f74a2c67ee5bb
Brandon Campbell <bmc1022@gmail.com>
2023-09-07 23:00:38 +1000

    custom devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,34 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations, :sessions], controllers: { sessions: "sessions" }
+  # Skip creation of the default :passwords, :registrations and :sessions Devise routes to avoid
+  # path duplication with the custom paths in the devise_scope block below.
+  # Use the specified custom user controllers instead of the default Devise controllers.
+  devise_for :users, skip: [:passwords, :registrations, :sessions], controllers: {
+    passwords: "users/passwords", registrations: "users/registrations", sessions: "users/sessions"
+  }
+
   devise_scope :user do
-    get  "login", to: "sessions#new", as: :new_user_session
-    post "login", to: "sessions#create", as: :user_session
-    delete "logout", to: "sessions#destroy"
+    # Authentication
+    get    "sign_up",        to: "users/registrations#new",     as: :new_user_registration
+    post   "sign_up",        to: "users/registrations#create",  as: :user_registration
+    get    "login",          to: "users/sessions#new",          as: :new_user_session
+    post   "login",          to: "users/sessions#create",       as: :user_session
+    delete "logout",         to: "users/sessions#destroy",      as: :destroy_user_session
+
+    # Password reset
+    get    "reset_password", to: "users/passwords#new",         as: :new_user_password
+    post   "reset_password", to: "users/passwords#create",      as: :user_password
+    get    "edit_password",  to: "users/passwords#edit",        as: :edit_user_password
+    patch  "edit_password",  to: "users/passwords#update",      as: :update_user_password
+    put    "edit_password",  to: "users/passwords#update"
+
+    # User registration
+    get    "close_account",  to: "users/registrations#cancel",  as: :cancel_user_registration
+    delete "close_account",  to: "users/registrations#destroy", as: :delete_user_registration
+    get    "edit_profile",   to: "users/registrations#edit",    as: :edit_user_registration
+    patch  "edit_profile",   to: "users/registrations#update",  as: :update_user_registration
+    put    "edit_profile",   to: "users/registrations#update"
   end
 
   authenticate :user do

2e53362a027faa7eae09f1bf2be1b22aeb6ed00c
Brandon Campbell <bmc1022@gmail.com>
2023-09-13 20:38:11 +1000

    application_policy specs

--- spec/policies/application_policy_spec.rb
+++ spec/policies/application_policy_spec.rb
@@ -5,8 +5,8 @@ require "rails_helper"
 RSpec.describe(ApplicationPolicy, type: :policy) do
   subject(:application_policy) { described_class.new(user, record) }
 
-  let(:user)   { build(:user)            }
-  let(:record) { instance_double(Gtrend) }
+  let(:user)   { build(:user)          }
+  let(:record) { instance_double(User) }
 
   describe "default permissions" do
     it { is_expected.to forbid_all_actions }
@@ -15,7 +15,7 @@ RSpec.describe(ApplicationPolicy, type: :policy) do
   describe ApplicationPolicy::Scope do
     subject(:scope)  { described_class.new(user, base_scope) }
 
-    let(:base_scope) { class_double(Gtrend) }
+    let(:base_scope) { class_double(User) }
 
     describe "#resolve" do
       it "raises a NotImplementedError" do

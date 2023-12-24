b4399e25fa286f22225a42cb4ab7049f7ca67cd8
Brandon Campbell <bmc1022@gmail.com>
2023-12-24 12:57:36 +1100

    add :has_role? method to guest model

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -6,4 +6,8 @@ class Guest
   def initialize(guest_id)
     @guest_id = guest_id.to_s
   end
+
+  def has_role?(role)
+    role == :guest
+  end
 end
--- spec/models/guest_spec.rb
+++ spec/models/guest_spec.rb
@@ -3,13 +3,27 @@
 require "rails_helper"
 
 RSpec.describe(Guest, type: :model) do
-  subject(:guest) { described_class.new(SecureRandom.uuid) }
+  subject(:guest) { build(:guest) }
 
   describe "attributes and indexes" do
     # Attributes
     describe ":guest_id" do
       it { is_expected.to respond_to(:guest_id) }
       it { expect(guest.guest_id).to be_a(String) }
+    end
+  end
+
+  describe "#has_role?" do
+    context "when role is :guest" do
+      it "returns true" do
+        expect(guest.has_role?(:guest)).to be(true)
+      end
+    end
+
+    context "when role is not :guest" do
+      it "returns false" do
+        expect(guest.has_role?(:registered)).to be(false)
+      end
     end
   end
 end

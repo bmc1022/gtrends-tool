b6248cf454c3d208f1f2b7b5297846e70ebf8cf5
Brandon Campbell <bmc1022@gmail.com>
2024-01-07 05:56:17 +1100

    update job enqueuing shared example

--- spec/support/shared_examples/jobs/job_enqueue.rb
+++ spec/support/shared_examples/jobs/job_enqueue.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples("job enqueuing") do
+RSpec.shared_examples("job enqueuing") do |queue_as: :default|
   describe "job enqueuing" do
     it "successfully enqueues #{described_class}" do
       expect {
         described_class.perform_later(*@job_arguments)
       }.to have_enqueued_job(described_class).with(*@job_arguments)
+    end
+  end
+
+  describe "queue as" do
+    it "is enqueued in the #{queue_as} queue" do
+      expect {
+        described_class.perform_later(*@job_arguments)
+      }.to have_enqueued_job.on_queue(queue_as)
     end
   end
 end

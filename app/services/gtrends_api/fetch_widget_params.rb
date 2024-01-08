d3bf3068fed4fe2786f1df12d81246f4c4fb56fb
Brandon Campbell <bmc1022@gmail.com>
2024-01-08 19:11:23 +1100

    rename method raw_widgets_data -> widgets_data

--- app/services/gtrends_api/fetch_widget_params.rb
+++ app/services/gtrends_api/fetch_widget_params.rb
@@ -15,14 +15,14 @@ class GtrendsApi::FetchWidgetParams < ApplicationService
 
   private
 
-  def raw_widgets_data
+  def widgets_data
     response = rescue_retry(HTTP.timeout(3).headers(cookie: @cookie).get(GENERAL_API_URL + @query))
     job_failed("Error fetching Google API tokens") unless response.is_a?(HTTP::Response)
     JSON.parse(response.to_s[4..])["widgets"] # Strip leading junk characters.
   end
 
   def time_series_params
-    time_series = raw_widgets_data.find { |widget| widget["id"] == "TIMESERIES" }
+    time_series = widgets_data.find { |widget| widget["id"] == "TIMESERIES" }
     { req: time_series["request"], token: time_series["token"], hl: "en-US", tz: "-600" }
   end
 end

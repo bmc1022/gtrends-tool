f35060163234789bebd8ebfe9ceed01fdf4afb4d
Brandon Campbell <bmc1022@gmail.com>
2024-01-08 19:21:05 +1100

    add user-agent header to interest_over_time_data request

--- app/services/gtrends_api/fetch_gtrends_data.rb
+++ app/services/gtrends_api/fetch_gtrends_data.rb
@@ -18,7 +18,7 @@ class GtrendsApi::FetchGtrendsData < ApplicationService
     widget_query = GtrendsApi::GenerateQuery.call(@keywords)
     params_with_token = GtrendsApi::FetchWidgetParams.call(widget_query)
     data_query = GtrendsApi::GenerateQuery.new(@keywords).build_query(params_with_token)
-    response = rescue_retry(HTTP.timeout(5).get(OVER_TIME_URL + data_query))
+    response = rescue_retry(HTTP.timeout(5).headers(user_agent).get(OVER_TIME_URL + data_query))
     job_failed("Error fetching trend data", return_value: {}) unless response.is_a?(HTTP::Response)
 
     over_time_averages(@keywords, JSON.parse(response.to_s[6..])) # Strip leading junk characters.
@@ -27,5 +27,14 @@ class GtrendsApi::FetchGtrendsData < ApplicationService
   def over_time_averages(keywords, data)
     averages = data["default"]["averages"]
     keywords.zip(averages).to_h
+  end
+
+  def user_agent
+    {
+      "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " \
+                      "AppleWebKit/537.36 (KHTML, like Gecko) " \
+                      "Chrome/120.0.0.0 " \
+                      "Safari/537.36"
+    }
   end
 end
